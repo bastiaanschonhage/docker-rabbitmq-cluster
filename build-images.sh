db0b7cc10ed2f4fa8bc09ac044efd43815d84add
Bastiaan Schönhage <bastiaan.schonhage@gmail.com>
2019-12-22 14:42:35 +0100

    Updating to RabbitMQ 3.8.2, with Erlang 22.2 and CentOS 8

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
-TAG="3.7.0"
+TAG="3.8.2"
 
-docker build -t bijukunjummen/rabbitmq-base:$TAG base
-docker build -t bijukunjummen/rabbitmq-server:$TAG server
+docker build -t bastiaanschonhage/rabbitmq-base:$TAG base
+docker build -t bastiaanschonhage/rabbitmq-server:$TAG server
